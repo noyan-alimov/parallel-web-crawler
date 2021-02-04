df1044f29867602730af91fc1ecdfe6c3c4c9810
Noyan <noyan.alimov@gmail.com>
2021-02-04 19:51:02 +0000

    initial commit

---
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/json/ConfigurationLoader.java
@@ -0,0 +1,53 @@
+package com.udacity.webcrawler.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+/**
+ * A static utility class that loads a JSON configuration file.
+ */
+public final class ConfigurationLoader {
+
+  private final Path path;
+
+  /**
+   * Create a {@link ConfigurationLoader} that loads configuration from the given {@link Path}.
+   */
+  public ConfigurationLoader(Path path) {
+    this.path = Objects.requireNonNull(path);
+  }
+
+  /**
+   * Loads configuration from this {@link ConfigurationLoader}'s path
+   *
+   * @return the loaded {@link CrawlerConfiguration}.
+   */
+  public CrawlerConfiguration load() {
+    try (Reader reader = Files.newBufferedReader(path)) {
+      return read(reader);
+    } catch (Exception e) {
+      e.getLocalizedMessage();
+      return null;
+    }
+  }
+
+  /**
+   * Loads crawler configuration from the given reader.
+   *
+   * @param reader a Reader pointing to a JSON string that contains crawler configuration.
+   * @return a crawler configuration
+   */
+  public static CrawlerConfiguration read(Reader reader) {
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      return objectMapper.readValue(reader, CrawlerConfiguration.class);
+    } catch (Exception e) {
+      e.getLocalizedMessage();
+      return null;
+    }
+  }
+}
